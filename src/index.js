e621d8e865876f4fcde42d7f62caba9ef8f37958
ioviic <ioviic@MacBook-Pro-ioviic.local>
2018-01-28 19:54:07 +0200

    add bcrypt jwt and response edited

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
-import express from 'express';
-import path from 'path';
+import express from "express";
+import path from "path";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import auth from "./routes/auth";
 
 const app = express();
+app.use(bodyParser.json());
+mongoose.connect("mongodb://127.0.0.1:27017/microBlocks");
 
-app.post('/api/auth', (req, res) => {
-  res.status(400).json({ errors: { global: 'Invalid Credentials' } });
-});
+app.use("/api/auth", auth);
 
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.listen(8080, () => {
-  console.log('Running on localhost:8080');
+  console.log("Running on localhost:8080 ");
 });
